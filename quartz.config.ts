20e7a8e248d497bb162833a8de0168383213e0d4
ingstudio <117122269+ingeniumstudio@users.noreply.github.com>
2025-02-02 05:16:02 -0300

    Quartz sync: Feb 2, 2025, 5:16 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,8 +8,8 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
-    pageTitleSuffix: "",
+    pageTitle: "🪴 kassius.ORG",
+    pageTitleSuffix: "by kassius yoke",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
