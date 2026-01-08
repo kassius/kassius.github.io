1e7b3df5b97f571ffc31652efc7ba9bc4e804458
ingstudio <117122269+ingeniumstudio@users.noreply.github.com>
2026-01-08 15:09:05 -0300

    Quartz sync: Jan 8, 2026, 3:09 PM

--- content/index.md
+++ content/index.md
@@ -2,3 +2,5 @@
 title: Kassius Yoke's Digital Garden
 ---
 this must be coming soon...
+
+k
--- sync.sh
+++ sync.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env zsh
 
-rm -rf ~/git-stuff/quartz/content/
-cp -r ~/other/shared/Documents/OBSIDIAN/content/ ~/git-stuff/quartz/
+# rm -rf ~/git-stuff/quartz/content/
+# rm -rf ~/git-stuff/kassius.github.io/content/
+# cp -r ~/other/shared/Documents/OBSIDIAN/content/ ~/git-stuff/quartz/
+# cp -r ~/other/shared/OBSIDIAN/kassius-github-io_content/ ~/git-stuff/quartz/
+cp -r ~/other/shared/OBSIDIAN/kassius-github-io_content/* ~/git-stuff/kassius.github.io/content/
+# npx quartz sync
 npx quartz sync
